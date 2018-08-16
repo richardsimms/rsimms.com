968d184a2293ee70f1b2d216355cf2e00c2df1c6
Richard Simms <rich@R-I-C-H--K-A-T-E.local>
2018-08-16 21:02:22 +1000

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <div>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Learn to create value in design</h1>
+          <p>Welcome to your new Gatsby site.</p>
+          <Link to="/video">Watch the video</Link>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default IndexPage
