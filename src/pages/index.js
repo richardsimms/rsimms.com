17780a9c3c22c7806b3cabe77511b3a93c7f100c
Richard Simms <rich@richkate.home>
2019-01-13 13:28:02 +1100

    SVG animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,15 @@ const IndexPage = () => (
           <h2>Design methods and principles that you can put to work right away on a product and a variety of projects.</h2>
           <Link to="/video">Learn it for free</Link>
           <p>Get the 7 days free email course</p>
-          <svg width="100%" height="172" viewBox="0 0 100% 172" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <div className="Logos">
+            <img src={require('../images/logo-sketch.png')} width="50px" />
+            <img src={require('../images/logo-figma.png')} width="50px" />
+            <img src={require('../images/logo-framer.png')} width="50px" />
+            <img src={require('../images/logo-studio.png')} width="50px" />
+            <img src={require('../images/logo-swift.png')} width="50px" />
+            <img src={require('../images/logo-react.png')} width="50px" />
+          </div>
+          <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill="white">
               <animate repeatCount="indefinite" fill="freeze" attributeName="d" dur="10s"
               values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
