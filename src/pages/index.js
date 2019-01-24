92c9067a1b1326e5870bbe560e9483f5185b4b90
Richard Simms <rich@richkate.home>
2019-01-24 19:53:09 +1100

    changed image and updated layout import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from "../components/layout";
+import Layout from '../components/layout';
 import Card from '../components/Card';
 import Summary from '../components/Summary';
 import Footer from '../components/Footer';
