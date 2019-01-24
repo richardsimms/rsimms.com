12be1c0d15045b881a75e502bec106780e9085bd
Richard Simms <richardsimms@RSimms.local>
2019-01-24 16:48:00 +1100

    tweak

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Layout from "../components/layout";
 import Card from '../components/Card';
 import Summary from '../components/Summary';
 import Footer from '../components/Footer';
 import Banner from '../components/Banner';
-import Layout from "../components/layout"
+
 
 const IndexPage = () => <Layout>
   <div>
