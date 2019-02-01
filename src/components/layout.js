22f8f67e8a6f9aa55dbf5d52f295f64f8a37c808
Richard Simms <rich@R-I-C-H--K-A-T-E.local>
2019-02-01 20:40:55 +1100

    Updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,13 +10,14 @@ const Layout = ({ children, data }) => (
     <Helmet>
       <meta charSet="utf-8" />
       <title>RSIMMS | Value-based Design</title>
+      <meta name="Description" content=" Richard Simms, a Design Lead. Over the last 15 years, I have designed and developed projects in the digital space, pretty much anything that has a screen." />
+
       <link rel="canonical" href="https://rsimms.com" />
+      <html lang="en" />
     </Helmet>
     <Header />
       {children}
-
   </div>
 )
-
 
 export default Layout
