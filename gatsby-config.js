424d8dd445be609d449eb1b29202725f35b2f10f
Richard Simms <rich@R-I-C-H--K-A-T-E.local>
2019-02-05 20:06:30 +1100

    logo change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'RSimms | Value-based Design',
-    description: 'Value courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
-    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
+    title: 'Richard Simms',
+    description: 'I design human-centered services, transforming ideas into digital products and creating unique customer experiences.',
+    keywords: 'UX, Design, Product, human-centered, UI, Strategy, value-based, service, Lean UX, App, Website, sketch, figma, prototypes, startup, '
   },
   plugins: [
     'gatsby-plugin-react-helmet',
