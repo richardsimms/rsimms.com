23aa7b25c76ed16e8e0c88ea3ce42d5798ec1535
Richard Simms <rich@R-I-C-H--K-A-T-E.local>
2019-08-09 13:00:52 +1000

    added gatsby-image to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Link from 'gatsby-link';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 
-const indexPage = () => <Layout>
+const indexPage = ({ data }) => <Layout>
   <div>
     <div className="Hero">
       <div className="HeroGroup">
@@ -17,22 +17,22 @@ const indexPage = () => <Layout>
       <Wave />
       </div>
     </div>
-    <div className="ContentGroup ContentTop">      
+    <div className="ContentGroup ContentTop">
       <h3>Listening to your customers is good business</h3>
       <p>I help businesses of all sizes to optimise their digital strategy, create and capture measurable outcomes by researching what their customers really want.</p>
        <p>I’ve learned that most digital businesses have little knowledge of what features actually solve the underlying business outcomes. So, I've created a reliable way for businesses to get started: by thinking about customers’ behaviour, responding to their needs, generating opportunities and fixing any experience issues.</p>
     </div>
     <div className="ImageOverlap">
-      <imageoverlap><img className="Image" src={require('../images/img-large-suncorp.jpg')} alt="Three screens from the Suncorp App" /></imageoverlap>
-      <imageoverlap><img className="Image" src={require('../images/img-small-appmap.jpg')} alt="Appmap of Suncorp App" /></imageoverlap>
+    <imageoverlap><Img className="Image" fluid={data.image1.childImageSharp.fluid} alt="Three screens from the Suncorp App" /></imageoverlap>
+    <imageoverlap><Img className="Image" fluid={data.image2.childImageSharp.fluid} alt="Appmap of Suncorp App" /></imageoverlap>
     </div>
     <div className="ContentGroup">      
       <h3>Most businesses get feature development wrong.</h3>
       <p>They fail to define success, or they don't know how the outcomes are routed back to their business goals. I work to a  process that draws on years of experience to make value-based design decisions. </p>
     </div>
     <div className="ImageBlock">
-      <img className="Image" src={require('../images/img-square-mastercard.jpg')} alt="Collection of Mastercard App screens" />
-      <img className="Image" src={require('../images/img-portrait-mastercard.jpg')} alt="Single Mastercard screen" />
+      <Img className="Image" fluid={data.image3.childImageSharp.fluid} alt="Collection of Mastercard App screens" />
+      <Img className="Image" fluid={data.image4.childImageSharp.fluid} alt="Single Mastercard screen" />
     </div>
     <div className="ContentGroup">      
       <h3>What’s new</h3>
@@ -42,8 +42,8 @@ const indexPage = () => <Layout>
       </ul>
     </div>
     <div className="ImageOverlap">
-      <imageoverlap><img className="Image" src={require('../images/img-large-et.jpg')} alt="Collection of components" /></imageoverlap>
-      <imageoverlap><img className="Image" src={require('../images/img-small-et.jpg')} alt="Close up of single component in iPad" /></imageoverlap>
+      <imageoverlap><Img className="Image" fluid={data.image5.childImageSharp.fluid} alt="Collection of components" /></imageoverlap>
+      <imageoverlap><Img className="Image" fluid={data.image6.childImageSharp.fluid} alt="Close up of single component in iPad" /></imageoverlap>  
     </div>
     <Summary 
       title="&#128075; Hello, nice to meet you!"
@@ -78,19 +78,56 @@ const indexPage = () => <Layout>
           />
         </div>
     </div>
+    
     <Footer />
   </div>
 </Layout>
 export default indexPage
 
 export const pageQuery = graphql`
-  query {
-    imageOne: file(relativePath: { eq: "img-large-suncorp.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 1000) {
-          ...GatsbyImageSharpFluid
-        }
+query {
+  image1: file(relativePath: { eq: "img-large-suncorp.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
       }
     }
   }
+  image2: file(relativePath: { eq: "img-small-appmap.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  image3: file(relativePath: { eq: "img-square-mastercard.jpg" }) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  image4: file(relativePath: { eq: "img-portrait-mastercard.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  image5: file(relativePath: { eq: "img-large-et.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  image6: file(relativePath: { eq: "img-small-et.jpg" }) {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  
+}
 `
