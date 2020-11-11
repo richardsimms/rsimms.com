62370e625a9124bce386418ee7f51b76a2371dec
richardsimms <richard@rsimms.com>
2020-11-12 10:41:41 +1100

    form updates

--- src/pages/clarity.js
+++ src/pages/clarity.js
@@ -9,14 +9,14 @@ const form = () => ( <Layout>
       <span>Start designing impact, not opinions.</span>
       <h1>Design for clarity when everything is uncertain </h1>    
       <h2>A free guide to teach you how to make profitable design decisions even in these crazy times. </h2> 
-       <form name="test" method="post" action="/success" data-netlify="true" netlify-honeypot="bot-field" data-netlify="true">
+       <form name="test" method="post" data-netlify="true">
          <p><label class="visuallyhidden">Name</label><input type="text" title="name" placeholder="Your name" name="name" /></p>
          <p><label class="visuallyhidden">Email</label><input type="email" placeholder="Your email" name="email" /></p>
          <p><button type="submit">SEND ME THE FREE GUIDE</button></p>
       </form> 
       
       <hr />
-      <form name="contact-new" method="POST" data-netlify="true" action="/success" >
+      <form name="contact-new" method="POST" data-netlify="true">
       <p>
         <label>Your Name: <input type="text" name="name" /></label>   
       </p>
