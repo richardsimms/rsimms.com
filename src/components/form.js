9427879b6ace2c037c65f5130e49c97540b566d4
richardsimms <richard@rsimms.com>
2020-11-13 20:07:52 +1100

    thank you page

--- src/components/form.js
+++ src/components/form.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../components/layout.css";
 
+
 export default function App() {
 
   return (
-    <form name="Newsletter" method="POST" netlify="true" action="/find-clarity-in-uncertainty.pdf">
+    <form name="Newsletter" method="POST" netlify="true" action="/success">
         <input type="hidden" name="form-name" value="Newsletter" />
         <p><label class="visuallyhidden">Your Name: <input type="text" name="name" placeholder="Your name" /></label></p>
         <p><label>What’s your best email address? <input type="email" name="email" placeholder="Your email" required /></label></p>
--- src/pages/success.js
+++ src/pages/success.js
@@ -1,11 +1,41 @@
+
 import React from 'react';
 import Layout from '../components/layout';
+import styled  from 'styled-components'
 
+
+const Success = styled.div`
+  h3 {
+    color: #FCF7F5;
+    font-size: 24px;
+    font-weight: 800;
+    margin: 50px;
+    font-family: 'Fira Sans', sans-serif;
+  }
+  h1 {
+    font-family: 'Fira Sans', sans-serif;
+    font-size: 24px;
+    line-height: 32px;
+    text-align: left;
+    font-weight: 800;
+    color: #580037;
+    margin-block-end: 0.5em;
+  }
+
+`;
 const success = () => ( <Layout>
      
-  <div className="HeroGroup">
-      <h1>Thanks</h1>    
+  <Success>
+    <div className="BannerThanks">
+        <div className="SectionTitleGroup">
+          <div><h3>Thanks!</h3></div>
+        </div>
+      </div>
+    <div className="ContentGroup">      
+      <h1>Thanks, <a href="/find-clarity-in-uncertainty.pdf">Download</a> your free guide here</h1>
     </div>
+
+    </Success>
 </Layout>
 )
 export default success
