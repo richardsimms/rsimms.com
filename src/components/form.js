d6167fad08f649f6658f211ec640eaf7d60900d5
richardsimms <richard@rsimms.com>
2020-11-13 16:46:33 +1100

    form working. Update form to PDF

--- src/components/form.js
+++ src/components/form.js
@@ -4,7 +4,7 @@ import "../components/layout.css";
 export default function App() {
 
   return (
-    <form name="Newsletter" method="POST" netlify="true" action="https://rsimms.s3-ap-southeast-2.amazonaws.com/Find-clarity-in-uncertainty.pdf">
+    <form name="Newsletter" method="POST" netlify="true" action="/find-clarity-in-uncertainty.pdf">
         <input type="hidden" name="form-name" value="Newsletter" />
         <p><label class="visuallyhidden">Your Name: <input type="text" name="name" placeholder="Your name" /></label></p>
         <p><label>What’s your best email address? <input type="email" name="email" placeholder="Your email" required /></label></p>
