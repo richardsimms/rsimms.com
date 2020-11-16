7b2566a0f8e81aa912988a8e927ae5cf3da806c5
richardsimms <richard@rsimms.com>
2020-11-16 21:52:48 +1100

    updated contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,40 +13,46 @@ const Contact = styled.div`
     grid-gap: 40px;
     position: relative;
     background: #1F4753;
+    background-color: #1F4753;
     width: 100%;
     height: 100vh;
     padding: 10px 0;
     margin: 0 auto; 
+    overflow: hidden;
+    scroll:no;
 
     h1 {
-        color: #FA5483;
+        color: #F8BFD0;
         font-size: 24px;
         font-height: 600;
     }  
 
     p {
-        color: #ffffff;
+        color: #FCF7F5;
         font-size: 18px;
         line-height: 1.3;
     }
+
+    a {
+        text-decoration:none;
+    }
+
 `
 const blank = styled.div``
 const info = styled.div``
 
 const contactPage = () => <Layout>
-    <div>
         <Contact>
         <blank></blank>
         <info>
             <h1>Richard Simms</h1>
             <p>Design Lead</p><br />
+            <hr />
             <a href="mailto:richard@rsimms.com"><p>richard@rsimms.com</p></a>
             <a href="sms://+61437116722"><p>+61 437 116 722</p></a>
             <a href="https://www.linkedin.com/in/richardsimms"><p>LinkedIn profile</p></a>
-            <a href="https://vbd.substack.com"><p>Publication and newsletter</p></a>
         </info>
     </Contact>
-    </div>
 </Layout>
 
 export default contactPage
