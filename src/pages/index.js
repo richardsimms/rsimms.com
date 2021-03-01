7e0aabd86ba48e93c1d99615aee384fe8ce9663c
richardsimms <richard@rsimms.com>
2021-03-01 14:19:05 +1100

    annoying now

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const indexPage = () => <Layout>
       </ul>
     </div>
     <div className="ImageSingle">
-      <img className="Image" src={require('../images/watering.jpg')} alt="Watering a plant and a phone on a table" /> 
+      {/* <img className="Image" src={require('../images/watering.jpg')} alt="Watering a plant and a phone on a table" />  */}
     </div>
     <div className="ContentBackground">
       <div className="ContentGroup">     
