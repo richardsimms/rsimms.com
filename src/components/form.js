d38c608d613a3d06f6589d97fd5bb9fe34915044
richardsimms <richard@rsimms.com>
2021-03-01 13:03:24 +1100

    Updated

--- src/components/form.js
+++ src/components/form.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <form name="Newsletter" method="POST" netlify="true" action="/success">
         <input type="hidden" name="form-name" value="Newsletter" />
-        <p><label class="visuallyhidden">Your Name: <input type="text" name="name" placeholder="Your name" /></label></p>
+        <p><label className="visuallyhidden">Your Name: <input type="text" name="name" placeholder="Your name" /></label></p>
         <p><label>What’s your best email address? <input type="email" name="email" placeholder="Your email" required /></label></p>
         <p><button type="submit">SEND ME THE FREE GUIDE</button></p>
       </form>
